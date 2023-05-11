8b6e759a4dcc28642f75a2d62e032448acf63339
Amit Nativ <amitnativ@menteebot.com>
2023-05-11 11:59:03 +0000

    added state_publisher v:0.3

--- src/state_publisher/src/state_publisher/launch/state_publisher.launch.py
+++ src/state_publisher/src/state_publisher/launch/state_publisher.launch.py
@@ -5,6 +5,7 @@ import os
 from launch.conditions import LaunchConfigurationEquals
 from launch.actions import DeclareLaunchArgument
 from ament_index_python import get_package_share_directory
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     
@@ -20,6 +21,12 @@ def generate_launch_description():
 
     urdf_file = DeclareLaunchArgument("urdf_file", default_value=[pkg_share, '/description/urdf/', 'happy.urdf'],
                                     description='Absolute path to robot urdf file')
+    
+    
+    publish_joint_states = DeclareLaunchArgument("publish_joint_states", default_value='True',    
+                                    description='Whether to publish the joint state or not')
+    
+    motors_params = os.path.join(get_package_share_directory('state_publisher'), 'configs', 'motors.yaml')
     
     joints_pub_rate = DeclareLaunchArgument("joints_pub_rate", default_value='10',
                                  description='The rate at which to publish updates to the /joint_states topic. Defaults to 10 [Hz]'
@@ -38,10 +45,27 @@ def generate_launch_description():
         }]
     )
 
+    robot_joint_publisher_node = launch_ros.actions.Node(
+        condition=IfCondition(LaunchConfiguration('publish_joint_states')),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_publisher',
+        namespace=LaunchConfiguration('namespace'),
+        parameters=[{
+            'robot_description': Command(['xacro ', LaunchConfiguration('urdf_file')]),
+            'rate': LaunchConfiguration('joints_pub_rate'),
+            'publish_default_positions': True,
+            },
+            motors_params],
+        
+    )
+
     launch_me.add_action(namespace)
     launch_me.add_action(urdf_file)
     launch_me.add_action(publish_frequency)
     launch_me.add_action(joints_pub_rate)
     launch_me.add_action(robot_state_publisher_node)
-    
+    launch_me.add_action(publish_joint_states)
+    launch_me.add_action(robot_joint_publisher_node)
+
     return launch_me
